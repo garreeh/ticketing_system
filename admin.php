3066a230fbc01851ee3b92ccab240543d52847ce
Garry <gajultos.garry123@gmail.com>
2023-12-28 12:24:34 +0800

    Added Assets

--- admin.php
+++ admin.php
@@ -39,7 +39,8 @@
                             <div class="col-lg-6">
                                 <div class="p-5">
                                     <div class="text-center">
-                                        <h1 class="h4 text-gray-900 mb-4">Admin Login</h1>
+                                        <h1 class="h4 text-gray-900 mb-4">Help Desk | Admin Login</h1>
+
                                     </div>
                                     <form class="user">
                                         <div class="form-group">
--- index.php
+++ index.php
@@ -32,14 +32,13 @@
             <div class="col-xl-10 col-lg-12 col-md-9">
 
                 <div class="card o-hidden border-0 shadow-lg my-5">
-                    <div class="card-body p-0">
+                    <div class="card-body">
                         <!-- Nested Row within Card Body -->
                         <div class="row">
-                            <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
-                            <div class="col-lg-6">
+                            <div class="col-lg-12">
                                 <div class="p-5">
                                     <div class="text-center">
-                                        <h1 class="h4 text-gray-900 mb-4">User Login</h1>
+                                        <h1 class="h4 text-gray-900 mb-4">Help Desk | User Login</h1>
                                     </div>
                                     <form class="user">
                                         <div class="form-group">
@@ -62,20 +61,12 @@
                                             Login
                                         </a>
                                         <hr>
-                                        <a href="index.html" class="btn btn-google btn-user btn-block">
-                                            <i class="fab fa-google fa-fw"></i> Login with Google
-                                        </a>
-                                        <a href="index.html" class="btn btn-facebook btn-user btn-block">
-                                            <i class="fab fa-facebook-f fa-fw"></i> Login with Facebook
-                                        </a>
+
                                     </form>
-                                    <hr>
                                     <div class="text-center">
                                         <a class="small" href="forgot-password.html">Forgot Password?</a>
                                     </div>
-                                    <div class="text-center">
-                                        <a class="small" href="register.html">Create an Account!</a>
-                                    </div>
+
                                 </div>
                             </div>
                         </div>
